89583bd5b6ea8f5818cdc60ccb940eaffc6170e9
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-01 17:09:06 -0500

    adding next and previous buttons, reorganized hiking section

--- hiking/test.js
+++ hiking/test.js
@@ -17,32 +17,13 @@ req.onload = function (event) {...};
 req.open('get', 'some-file.txt', true);
 req.send(); */
 
-alert('Hello World');
-
-//from: https://stackoverflow.com/questions/26443828/press-left-and-right-arrow-to-change-image
-/* var imgs = ["images/20140621.jpg", "images/20140621_150846.jpg"];
-
-function changeImage(dir) {
-    var img = document.getElementById("imgClickAndChange");
-    img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
-}
-
-document.onkeydown = function(e) {
-    e = e || window.event;
-    if (e.keyCode == '37') {
-        changeImage(-1) //left <- show Prev image
-    } else if (e.keyCode == '39') {
-        // right -> show next image
-        changeImage()
-	}
-} */
-
+//alert('Hello World');
 
 //from: Adapted from David Thomas at Stack Exchange 
 //https://stackoverflow.com/questions/26443828/press-left-and-right-arrow-to-change-image
 function imgCycle(e) {
     // obviously use your own images here:
-    var imgs = ["20140621.jpg", "20140621_150846.jpg"],
+    var imgs = ["1.jpg", "2.jpg"],
         // reference to the img element:
         imgElement = document.getElementById('imgClickAndChange'),
         // finding the string of the src after the last '/' character:
@@ -51,7 +32,6 @@ function imgCycle(e) {
         curIndex = imgs.indexOf(curImg),
         // initialising the nextIndex variable:
         nextIndex;
-
     // if we have a keyCode (therefore we have a keydown or keyup event:
     if (e.keyCode) {
         // we do different things based on which key was pressed:
@@ -71,23 +51,18 @@ function imgCycle(e) {
                 break;
 			default:
 				nextIndex = curIndex;
+				break;
         }
         // if we don't have a keyCode, we check if the event-type (in lowercase)
         // is a mousedown:
-    } else if (e.type.toLowerCase() === 'mousedown') {
-        // I'm assuming you'd like to advance to the next image, rather
-        // than regress to the previous (same check as above):
-        nextIndex = curIndex + 1 === imgs.length ? 0 : curIndex + 1;
     } else {
 		nextIndex = curIndex;
 	}
     // setting the src of the image to the relevant URL + the string from the array
     // at the 'nextIndex':
-    imgElement.src = 'images/' + imgs[nextIndex];
+    imgElement.src = imgs[nextIndex];
 }
 
-// binding a mousedown event-handler to the 'img' element:
-document.getElementById('imgClickAndChange').addEventListener('mousedown', imgCycle);
 // binding the keydown event-handler to the 'body' element:
 document.body.addEventListener('keydown', imgCycle);
        
