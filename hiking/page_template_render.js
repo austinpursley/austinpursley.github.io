55adf6a3082bc8f9c7980fd88d45b4daabc8ab83
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-11 01:18:23 -0500

    i think I made a dumb mistake

--- hiking/page_template_render.js
+++ hiking/page_template_render.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	var elem = document.getElementById("location");
 	var loc = elem.innerText;
 	alert("this far 0a");
-	$.getJSON("/../hiking_data.json", function(data) {
+	$.getJSON("hiking_data.json", function(data) {
 		alert("this far 1");
 		$.each(data, function(key, value){
 			alert(key);
