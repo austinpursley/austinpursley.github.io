78ae43c5393a15c242ff1db5d879b68a1e34be47
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-11 01:29:16 -0500

    script in hiking but directory still in colorado_bend. try /../hiking/hiking_data.json

--- hiking/page_template_render.js
+++ hiking/page_template_render.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	var elem = document.getElementById("location");
 	var loc = elem.innerText;
 	alert("this far 0b");
-	$.getJSON("hiking_data.json", function(data) {
+	$.getJSON("/../hiking/hiking_data.json", function(data) {
 		alert("this far 1");
 		$.each(data, function(key, value){
 			alert(key);
