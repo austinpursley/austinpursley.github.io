6718f2f75b292be1301671c44998d7a9c78765ea
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-11 00:07:41 -0500

    trying the mustache templates and stuff online

---
+++ hiking/index_template_render.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+	$.getJSON('hiking_data.json', function(data) {
+		var template = $('#hike_index_template').html();
+		var html = Mustache.to_html(template, data);
+		$('#templateArea').html(html);
+	});
+
+});
---
+++ hiking/page_template_render.js
@@ -0,0 +1,34 @@
+
+/* $(document).ready(function() {
+
+	$.getJSON('hiking_data.json', function(data) {
+		var template = $('#hiking_page_template').html();
+		var html = Mustache.to_html(template, data);
+		$('#templateArea').html(html);
+	});
+
+}); */
+
+$(document).ready(function() {
+	
+	var elem = document.getElementById("location");
+	var loc = elem.innerText;
+	alert("this far 0");
+	$.getJSON("/../hiking_data.json", function(data) {
+		alert("this far 1");
+		$.each(data, function(key, value){
+			alert("this far 2");
+			$.each(value, function(key, value){
+				alert("this far 3");
+				if (loc == key) {
+					alert("holy shit it worked");
+				}
+			});
+		});
+		
+		var template = $('#hiking_page_template').html();
+		var html = Mustache.to_html(template, data);
+		$('#templateArea').html(html);
+	});
+	
+});
