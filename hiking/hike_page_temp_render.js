f0ef6883fc2c379b34d26230ebb6acde7c2308f9
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-18 20:28:51 -0500

    external templates work

--- hiking/hike_page_temp_render.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	$.getJSON('hiking_data.json', function(data) {
-		var template = $('#hike_page_template').html();
-		var html = Mustache.to_html(template, data);
-		$('#templateArea').html(html);
-	});
-
-});
--- hiking/page_template_render.js
+++ hiking/page_template_render.js
@@ -1,29 +1,21 @@
-
-/* $(document).ready(function() {
-
-	$.getJSON('hiking_data.json', function(data) {
-		var template = $('#hiking_page_template').html();
-		var html = Mustache.to_html(template, data);
-		$('#templateArea').html(html);
-	});
-
-}); */
-
 $(document).ready(function() {
 	var elem = document.getElementById("loc");
 	var hike_loc = elem.innerText;
+		
 	// $.getJSON("/../hiking/hiking_data.json", function(data) {
 	$.getJSON("hiking_data.json", function(data) {
 		$.each(data.hiking_places, function(key, value1){
 			$.each(value1.place, function(key, value2){
 				if (hike_loc == value2.title2) {
-					var template = $('#hiking_page_template').html();
-					var html = Mustache.to_html(template, value2);
-					$('#templateArea').html(html);
-					return
-				};
+					$.get('templates.html', function(templates) {
+						var template = $(templates).find('#hiking_page_template').html();
+						var html = Mustache.to_html(template, value2);
+						$('#templateArea').html(html);
+						return
+					});
+					
+				}
 			});
 		});
 	});
-	
 });
