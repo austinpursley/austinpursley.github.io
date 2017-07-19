be9005f85600ce134ca60d69cf4a60d934b73353
Austin Pursley <austin.t.pursley@gmail.com>
2017-07-19 11:54:36 -0500

    changed 'hiking' to 'hikes'

--- hikes/page_template_render.js
+++ hikes/page_template_render.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 	var elem = document.getElementById("loc");
 	var hike_loc = elem.innerText;
 	
-	// /../hiking/hiking_data.json only works offline
-	$.getJSON("/../hiking/hiking_data.json", function(data) {
+	// /../hikes/hiking_data.json only works offline
+	$.getJSON("/../hikes/hiking_data.json", function(data) {
 	//$.getJSON("hiking_data.json", function(data) {
-		$.each(data.hiking_places, function(key, value){
+		$.each(data.hiking_places, function(key, value) {
 			if (hike_loc == value.title2) {
-				$.get('/../hiking/hike_page_template.html', function(templates) {
+				$.get('/../hikes/hike_page_template.html', function(templates) {
 				//$.get('templates.html', function(templates) {
-				// /../hiking/hiking_data.json only works offline
+				// /../hikes/hiking_data.json only works offline
 					var template = $(templates).find('#hiking_page_template').html();
 					var html = Mustache.to_html(template, value);
 					$('#templateArea').html(html);
