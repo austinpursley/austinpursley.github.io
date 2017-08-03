f6eabc23aa637f25176d89239b0f7738b8edf763
Austin Pursley <austin.t.pursley@gmail.com>
2017-08-03 17:07:05 -0500

    got img contain working

--- hikes/hikes_main.js
+++ hikes/hikes_main.js
@@ -22,17 +22,18 @@ function render_page_templ(id) {
 				history.replaceState( { 
 					id: place.id
 				}, null, "/hikes/#" + place.id);
-				
+				window.addEventListener("resize", contain_img);
+				document.body.addEventListener('keydown', imgCycle);
 			}
 		});
 	});
 	return false;
 }
 
-window.addEventListener("resize", contain_img);
+
 
 window.onpopstate = function (event) { 
-  if (event.state.id == "index"){
+  if (event.state && event.state.id == "index"){
 	window.location.href = "hikes.html";
   }
 }
@@ -47,7 +48,6 @@ document.getElementById("templateArea").addEventListener("click", function(e) {
 	
 	if(e.target && e.target.nodeName == "BUTTON") {
 		imgCycle(e);
-		contain_img();
 	}
 });
 
@@ -55,82 +55,102 @@ document.getElementById("templateArea").addEventListener("click", function(e) {
 //https://stackoverflow.com/questions/26443828/press-left-and-right-arrow-to-change-image
 
 function imgCycle(e) { //e is the event.
-	var hike_id = document.getElementById('hike_id').innerHTML;
-	$.getJSON("hiking_data.json", function(data) {
-		$.each(data.hiking_places, function(key, place) {
-			if (hike_id == place.id) {
-				var imgs = place.img_array;
-				// reference to the img element:
-				var imgElement = document.getElementById('imgClickAndChange'),
-				// finding the string of the src after the last '/' character:
-				curImg = imgElement.src.split('/').pop(),
-				// finding that string from the array of images:
-				curIndex = imgs.indexOf(curImg),
-				// initialising the nextIndex variable:
-				nextIndex;
-
-				// if we have a keyCode (therefore we have a keydown or keyup event:
-				if (e.keyCode) {
-					// we do different things based on which key was pressed:
-					switch (e.keyCode) {
-						// keyCode 37 is the left arrow key:
-						case 37:
-							// if the current index is 0 (the first element in the array)
-							// we next show the last image in the array, at position 'imgs.length - 1'
-							// otherwise we show the image at the previous index:
-							nextIndex = curIndex === 0 ? (imgs.length - 1) : curIndex - 1;
-							break;
-							// keyCode 39 is the right arrow key:
-						case 39:
-							// if curIndex + 1 is equal to the length of the images array,
-							// we show the first element from the array, otherwise we use the next:
-							nextIndex = curIndex + 1 === imgs.length ? 0 : curIndex + 1; 
-							break;
-						default:
-							nextIndex = curIndex; 
-							break;
+	left_arrow_key = (e.keyCode == 37);
+	right_arrow_key = (e.keyCode == 39);
+	pre_button = (e.target.className == 'previous');
+	nxt_button = (e.target.className == 'next');
+	if (left_arrow_key || right_arrow_key || pre_button || nxt_button) {
+		var hike_id = document.getElementById('hike_id').innerHTML;
+		$.getJSON("hiking_data.json", function(data) {
+			$.each(data.hiking_places, function(key, place) {
+				if (hike_id == place.id) {
+					
+					var imgs = place.img_array,
+					imgElement = document.getElementById('imgClickAndChange'),
+					curImg = imgElement.src.split('/').pop(),
+					curIndex = imgs.indexOf(curImg),
+					nextIndex;
+					
+					if (left_arrow_key || pre_button) {
+						nextIndex = curIndex === 0 ? (imgs.length - 1) : curIndex - 1;
 					}
-				} 
-				else if (e.target.className == 'previous') {
-					nextIndex = curIndex === 0 ? (imgs.length - 1) : curIndex - 1; 
-				} 
-				else if (e.target.className == 'next') {
-					nextIndex = curIndex + 1 === imgs.length ? 0 : curIndex + 1; 
+					else if (right_arrow_key || nxt_button) {
+						nextIndex = curIndex + 1 === imgs.length ? 0 : curIndex + 1; 
+					}
+					else {
+						nextIndex = curIndex;
+					}
+					if (nextIndex != curIndex) {
+						imgElement.src = hike_id + "/" + imgs[nextIndex];
+						contain_img();
+					}
 				}
-				else {
-					nextIndex = curIndex;
-				}
-				imgElement.src = hike_id + "/" + imgs[nextIndex];
-				return false;
-			}
-		});	
-	});
- 	/*	if (numImg == 1) {
-		return false;
-	} */
+			});	
+		});
+	}
 }
 
-document.body.addEventListener('keydown', imgCycle);
+
 
 function contain_img() {
-
+	//To-Do: hide image when loading, maybe hide scrollbars, fix button on bottom of screen.
 	var img = document.getElementById('imgClickAndChange');
-	var img_ar = img.offsetWidth / img.offsetHeight;
-	var win_ar = window.innerWidth / window.innerHeight;
+	var title_width = document.getElementById('title').offsetWidth;
+	var win_w = window.innerWidth;
+	var win_h = window.innerHeight;
+	var win_ar = win_w / win_h;
+	var img_w = img.offsetWidth;
+	var img_h = img.offsetHeight;
+	var img_ar = img_w / img_h;
 	var thresh = 1.0;
-/* 	console.log("img_ar", img_ar);
-	console.log("win_ar", win_ar);
-	console.log("img src", img.src); */
-	if (win_ar < thresh) {
-		document.getElementById("img_style").href = "narrow_win_img_style.css";
-		// console.log('narrow');
-	}
-	
-	else if (win_ar > thresh) {
-		document.getElementById("img_style").href = "wide_win_img_style.css";
-		// console.log('wide');
-	}
-
+	setTimeout(function(){
+		console.log("img_ar", img_ar);
+		console.log("win_ar", win_ar);
+	//save_img_display = img.style.display;
+	//img.style.display = 'none';
+		var img_style = document.getElementById("img_style").href.split('/').pop();
+		if (win_ar > thresh) { // wide window
+			
+			if (img_ar < thresh) { //narrow image
+				document.getElementById("img_style").href = "wide_win_img_style.css";
+				//console.log('narrow image, wide sytle (1a)');
+			}
+			else { //wide image
+				if ((img_w > win_w) && (img_style == "wide_win_img_style.css")) {
+					document.getElementById("img_style").href = "narrow_win_img_style.css";
+					//console.log('narrow style (2b)');
+				}
+				
+				else if ( img_h + 150 > win_h && (img_style == "narrow_win_img_style.css")) {
+					document.getElementById("img_style").href = "wide_win_img_style.css";
+					//console.log('wide sytle (3c)');
+				}
+			}
+		}
+		else { // narrow window
+			if (img_ar > thresh) { //wide image
+				document.getElementById("img_style").href = "narrow_win_img_style.css";
+				//console.log('narrow image, wide sytle (1b)');
+			}
+			else { //narrow image
+				if ((img_h + 150 > win_h) && (img_style == "narrow_win_img_style.css")) {
+						document.getElementById("img_style").href = "wide_win_img_style.css";
+						//console.log('narrow style (2b)');
+					}
+					
+					else if ( img_w > win_w && (img_style == "wide_win_img_style.css")) {
+						document.getElementById("img_style").href = "narrow_win_img_style.css";
+						//console.log('wide sytle (3b)');
+					}
+			}
+		}
+		if (title_width < 550) {
+			document.getElementById('title').style.visibility = "hidden";
+		}
+		else if(document.getElementById('title').style.visibility == "hidden") {
+			document.getElementById('title').style.visibility = "visible"
+		}
+	}, 100);
 	return false;
 }
 
