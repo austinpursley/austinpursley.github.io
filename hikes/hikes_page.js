1f07d281779cfdb503b254d60cf2c5489c94901b
Austin Pursley <austin.t.pursley@gmail.com>
2017-08-20 18:09:30 -0500

    hikes page, hiding images while they are changing size

--- hikes/hikes_page.js
+++ hikes/hikes_page.js
@@ -1,13 +1,24 @@
+document.getElementById('hiking_page').style.overflow = "hidden";
+
+//wait until image loads to contain it and make it visible
 window.onload = function(){
 	contain_img();
+	hide_title();
 };
 
 $(document).ready(function() {
-	window.addEventListener("resize", contain_img);
+	//listen for event of a windows resize and keyboard input
+	window.addEventListener("resize", function() {
+		contain_img();
+		hide_title();
+	});
 	document.body.addEventListener('keydown', imgCycle);
+	//hide the scrollbar
+	document.getElementById('hiking_page').style.overflow = "hidden";
+	document.getElementById('imgClickAndChange').style.visibility = 'visible';
 });
 
-// Even listener for hike page click and image cycle buttons.
+// Even listener for image cycle buttons.
 //This comes from David Walsh's website article "How JavaScript Even Delegation Works"
 //https://davidwalsh.name/event-delegate
 document.getElementById("hiking_page").addEventListener("click", function(e) {
@@ -19,8 +30,8 @@ document.getElementById("hiking_page").addEventListener("click", function(e) {
 //Adapted from David Thomas's code on Stack Exchange 
 //https://stackoverflow.com/questions/26443828/press-left-and-right-arrow-to-change-image
 function imgCycle(e) { //e is the event.
-	left_arrow_key = (e.keyCode == 37);
-	right_arrow_key = (e.keyCode == 39);
+	left_arrow_key = (e.keyCode == 37); //left arrow key
+	right_arrow_key = (e.keyCode == 39); //right arrow key
 	pre_button = (e.target.className == 'previous');
 	nxt_button = (e.target.className == 'next');
 	
@@ -46,10 +57,13 @@ function imgCycle(e) { //e is the event.
 						nextIndex = curIndex;
 					}
 					
+					document.getElementById('imgClickAndChange').style.visibility = 'hidden';
+					
 					var newImg = new Image();
 					newImg.onload = function() {
 						imgElement.src = this.src;
 						contain_img();
+						document.getElementById('imgClickAndChange').style.visibility = 'visible';
 					};
 					
 					if (nextIndex != curIndex) {
@@ -62,76 +76,59 @@ function imgCycle(e) { //e is the event.
 }
 
 function contain_img() {	
-		//document.getElementById('imgClickAndChange').style.visibility = 'hidden';
-		//setTimeout(function(){
-		//$(document).ready(function() {
-		//$(window).on("load", function() {
-		var win_w = window.innerWidth;
-		var win_h = window.innerHeight;
-		var win_ar = win_w / win_h; //aspect ratio
-		var thresh = 1.0; //threshold for if aspect ratio is wide or narrow	
-		document.getElementById('hiking_page').style.overflow = "hidden";	
-		//grab variable values form html elements
-		var title_width = document.getElementById('title').offsetWidth;
-		var img = document.getElementById('imgClickAndChange');
-		var img_w = img.offsetWidth;
-		var img_h = img.offsetHeight;
-		var img_h_tol = img_h + 150; //tolerance for header and button heights
-		var img_ar = img_w / img_h;
-		//determine whether win and img are wide / narrow
-		wide_window = win_ar > thresh;
-		narrow_window = win_ar < thresh;
-		wide_img = img_ar > thresh;
-		narrow_img = img_ar < thresh;
-		//current img style
-		var img_style = document.getElementById("img_style").href.split('/').pop();
-		//set style based on img and window size
-		if (win_ar > thresh) { // wide window
-			
-			if (img_ar < thresh) { //narrow image
-				document.getElementById("img_style").href = "wide_win_img_style.css";
-				//console.log('narrow image, wide sytle (1a)');
-			}
-			else { //wide image
-				if ((img_w > win_w) && (img_style == "wide_win_img_style.css")) {
-					document.getElementById("img_style").href = "narrow_win_img_style.css";
-					//console.log('narrow style (2b)');
-				}
-				
-				else if ( img_h + 150 > win_h && (img_style == "narrow_win_img_style.css")) {
-					document.getElementById("img_style").href = "wide_win_img_style.css";
-					//console.log('wide sytle (3c)');
-				}
-			}
+	//get info on current image dimenstions
+	var img = document.getElementById('imgClickAndChange');
+	var img_w = img.offsetWidth;
+	var img_h = img.offsetHeight;
+	var img_ar = img_w / img_h;  //aspect ratio
+	//get info on window dimensions
+	var win_w = window.innerWidth;
+	var win_h = window.innerHeight;
+	var win_ar = win_w / win_h; 
+	//threshold for if aspect ratio is wide or narrow	
+	var thresh = 1.0; 	
+	//tolerance for header and button heights
+	var img_h_tol = img_h + 150; 
+	//css settings for img currently in use
+	var img_style = document.getElementById("img_style").href.split('/').pop();
+	//set style based on img and window size
+	if (win_ar > thresh) { // wide window
+		if (img_ar < thresh) { //narrow image
+			document.getElementById("img_style").href = "wide_win_img_style.css";
 		}
-		else { // narrow window
-			if (img_ar > thresh) { //wide image
+		else { //wide image
+			if ((img_w > win_w) && (img_style == "wide_win_img_style.css")) {
 				document.getElementById("img_style").href = "narrow_win_img_style.css";
-				//console.log('narrow image, wide sytle (1b)');
 			}
-			else { //narrow image
-				if ((img_h + 150 > win_h) && (img_style == "narrow_win_img_style.css")) {
-						document.getElementById("img_style").href = "wide_win_img_style.css";
-						//console.log('narrow style (2b)');
-					}
-					
-					else if ( img_w > win_w && (img_style == "wide_win_img_style.css")) {
-						document.getElementById("img_style").href = "narrow_win_img_style.css";
-						//console.log('wide sytle (3b)');
-					}
+			else if ( img_h_tol > win_h && (img_style == "narrow_win_img_style.css")) {
+				document.getElementById("img_style").href = "wide_win_img_style.css";
 			}
 		}
-		//hide the title if screen is too narrow
-		if (title_width < 600) {
-			document.getElementById('title').style.visibility = "hidden";
+	}
+	else { // narrow window
+		if (img_ar > thresh) { //wide image
+			document.getElementById("img_style").href = "narrow_win_img_style.css";
 		}
-		//display title if it was hidden but screen is wide
-		else if(document.getElementById('title').style.visibility == "hidden") {
-			document.getElementById('title').style.visibility = "visible"
+		else { //narrow image
+			if ( img_w > win_w && (img_style == "wide_win_img_style.css")) {
+				document.getElementById("img_style").href = "narrow_win_img_style.css";
+			}
+			else if ((img_h_tol > win_h) && (img_style == "narrow_win_img_style.css")) {
+					document.getElementById("img_style").href = "wide_win_img_style.css";
+			}
 		}
-		//img.style.visibility = 'visible';
-		document.getElementById('hiking_page').style.overflow = "visible";
-	//});
-	//}, 200);
+	}
 	return false;
+}
+
+function hide_title() {
+	//hide the title if screen is too narrow
+	var title_width = document.getElementById('title').offsetWidth;
+	if (title_width < 600) {
+		document.getElementById('title').style.visibility = "hidden";
+	}
+	//display title if it was hidden and screen is wide enough
+	else if(document.getElementById('title').style.visibility == "hidden") {
+		document.getElementById('title').style.visibility = "visible"
+	}
 }
