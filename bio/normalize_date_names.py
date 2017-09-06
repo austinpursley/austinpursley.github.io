38c41fb606324962f42b74e9d74a0b5cd0a0b34c
Austin Pursley <austin.t.pursley@gmail.com>
2017-09-06 11:02:37 -0500

    loose ends

--- bio/normalize_date_names.py
+++ bio/normalize_date_names.py
@@ -16,8 +16,7 @@ for file in file_list:
         f = f_list[0]
         date = f[:15]
         if (is_number(date) == True):
-            newdate = (date[:4] + "." + date[4:6] + "." + date[6:8] +
-                       date[8:11] + "." + date[11:13] + "." + date[13:15])
+            newdate = (date[:4] + "." + date[4:6] + "." + date[6:8])
             os.rename(text_path + "/image_name_fix/" + file, text_path +
                       "/image_name_fix/" + newdate + ".jpg")
 
