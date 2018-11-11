09c72f3113335244843c052232d59d8ed7389ce6
Austin Pursley <austin.t.pursley@gmail.com>
2018-11-11 03:20:12 -0500

    add squawk python script

--- squawk/add_squawk.py
+++ squawk/add_squawk.py
@@ -1,21 +1,21 @@
 import json
-data = {}
-data['set'] = []
+import csv
+from datetime import datetime
 
-for file in os.listdir():
-    splitpath = os.path.splitext(file)
-    if splitpath[1] == ".html":
-        id = splitpath[0]
-        print(id)
-        date = file[-17:-5]
-        print(date)
-        HtmlFile = open(file, 'r', encoding='utf-8')
-        text = HtmlFile.read()
-        print(text)
-        data['set'].append({
-            'text': text,
-            'date': date,
-            'id': id
-        })
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+with open('data.csv','a', newline='') as data:
+    date = input("squawk date: ")
+    text = input("squawk: ")
+    sqwk_id = "squawk_" + date
+    print(date, text, sqwk_id)
+    new_squawk_row = [text, date, sqwk_id]
+    writer = csv.writer(data)
+    writer.writerow(new_squawk_row)
+data.close()
+
+data = csv.reader(open('data.csv', 'r', newline=''))
+next(data, None)
+sortdata = sorted(data, key=lambda row: datetime.strptime(row[1], '%Y%m%d%H%M%S'))
+
+with open('data_sorted.csv', 'w', newline='') as newfile:
+    writer = csv.writer(newfile)
+    writer.writerows(sortdata)
