2f7b07f004147a8622e1fd25f7205e5ffc97ef9c
Austin Pursley <austin.t.pursley@gmail.com>
2019-04-27 17:26:35 -0400

    questions page work: working on update python script

--- questions/update_html.py
+++ questions/update_html.py
@@ -1,8 +1,12 @@
 import os
 import csv
+import lxml
+from bs4 import BeautifulSoup
 from datetime import datetime
 
 def update_html():
+    
+    # update question index
     html_index_start = """
 		<!DOCTYPE html>
 		<html>
@@ -24,10 +28,10 @@ def update_html():
     html_index_mid_temp = """
         <tr>
             <td>
-			    <time><a href="question_page">date</a></time>  
+			    <time>date</time>  
             </td>
 			<td>
-				question_text
+				<a href="question_page">question_text</a>
 			</td>
         </tr>
     """
@@ -41,25 +45,7 @@ def update_html():
         </html>
     """
 
-    html_page_temp = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title> q_id </title>
-            <link rel="stylesheet" href="../style.css">
-            <link rel="stylesheet" href="../mobile.css" media="screen and (max-device-width: 800px)" />
-        </head>
-        <body id="question_page">
-            <div id="question">
-                <h1>question_text</h1>
-            </div>
-            <time>question_date</time>
-            <div id="answer">
-            </div>
-        </body>
-        </html>
-    """
-    # update question index
+    pages = []
     with open('data.csv', "r+", newline='') as data:
         reader = csv.reader(data)
         index_mid = ""
@@ -68,14 +54,15 @@ def update_html():
             text = text.replace("\\n", "")
             date = row[0]
             datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-            index_date_format = datetime.strftime(datetime_obj, '%Y.%m.%d %I:%M %p')
+            date_format = datetime.strftime(datetime_obj, '%Y.%m.%d %I:%M %p')
             date_html = datetime.strftime(datetime_obj, '%Y%m%d-%H%M%S')
             id = 'question_' + date_html
             file_name =  id + '.html'
             index_mid_part = html_index_mid_temp.replace("question_page", file_name)
-            index_mid_part = index_mid_part.replace("date", index_date_format)
+            index_mid_part = index_mid_part.replace("date", date_format)
             index_mid_part = index_mid_part.replace("question_text", text)
             index_mid += index_mid_part
+            pages.append(file_name)
         
         html_str = html_index_start + index_mid + html_index_end
         Html_file = open("questions.html", "w")
@@ -83,7 +70,25 @@ def update_html():
         Html_file.close()
 
     # update question pages
-    pages = []
+    html_page_temp = """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title> q_id </title>
+                <link rel="stylesheet" href="../style.css">
+                <link rel="stylesheet" href="../mobile.css" media="screen and (max-device-width: 800px)" />
+            </head>
+            <body id="question_page">
+                <h1><div id="question">
+                    question_text
+                </div></h1>
+                <time>question_date</time>
+                <div id="answer">
+                </div>
+            </body>
+            </html>
+        """
+        
     with open('data.csv', "r+", newline='') as data:
         reader = csv.reader(data)
         for row in reader:
@@ -92,13 +97,37 @@ def update_html():
             text = text.replace("\\n", "")
             date = row[0]
             datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            date_format = datetime.strftime(datetime_obj, '%Y.%m.%d %I:%M %p')
             date_html = datetime.strftime(datetime_obj, '%Y%m%d-%H%M%S')
             id = 'question_' + date_html
             file_name =  id + '.html'
-            pages.append(file_name)
-            html_str = html_page_temp.replace("q_id", id)
-            html_str = html_str.replace("question_text", text)
-            html_str = html_str.replace("question_date", date)
+            if os.path.isfile(file_name):
+                print(file_name)
+                with open(file_name) as fp:
+                    soup = BeautifulSoup(fp, 'lxml')
+                    soup.time.string = date_format
+                    soup.title.string = id
+                    question = soup.find("div", {"id": "question"})
+                    question.string = text
+                    html_str = soup.prettify()
+                    print(soup.prettify()) 
+                    # soup.time.replace_with(date)
+                    # soup.find("div", {"id": "question"}).replace_with(question)
+                    
+                    # html_str = BeautifulSoup(fp, 'lxml')
+                    # html_str = soup.replace("q_id", id)
+                    # html_str = html_str.replace("question_text", text)
+                    # html_str = html_str.replace("question_date", date)
+                
+                # new_title = id
+                # new_date = date
+                # new_question = 
+                # print(title, date, question)
+                # continue
+            else:
+                html_str = html_page_temp.replace("q_id", id)
+                html_str = html_str.replace("question_text", text)
+                html_str = html_str.replace("question_date", date)
             Html_file = open(file_name, "w")
             Html_file.write(html_str)
             Html_file.close()
