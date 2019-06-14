ff5692a5f6380c503b320ab876e216af95f90f7e
Austin Pursley <austin.t.pursley@gmail.com>
2019-06-14 11:39:47 -0400

    misc reorganization and clean-up

--- questions/update_data.py
+++ questions/update_data.py
@@ -2,9 +2,15 @@ import csv
 from datetime import datetime
 
 def update_data():
-    # with open('data.csv') as data:
-        # numline = sum(1 for row in data)
-        # print('number of question before: ' + str(numline))
+    # get new index number
+    with open('data.csv', "r+", newline='') as data:
+        reader = csv.reader(data)
+        #returns row with highest column 3 (index)
+        maxnum = max(reader, key=lambda row: int(row[2])) 
+        new_index = int(maxnum[2]) + 1
+        print("new question index: " + str(new_index))
+    
+    # get data for new question from user and write to csv file
     with open('data.csv', 'a', newline='') as data:
         date_in = input("\nQuestion Date?:\noption1: %Y-%m-%d %H:%M\noption2: 'today' or 'now' for current timestamp\n\nDATE: ")
         if date_in ==  "today" or date_in == 'now':
@@ -15,19 +21,16 @@ def update_data():
         print(date)
         question = input("questions: ")
         print(date, question)
-        new_question_row = [date, question]
+        new_question_row = [date, question, new_index]
         writer = csv.writer(data)
         writer.writerow(new_question_row)
-
-    # with open('data.csv', 'r', newline='') as data:
-        # numline = sum(1 for row in data)
-        # print('number of question after: ' + str(numline))
     
     with open('data.csv', 'r', newline='') as data:
         # reader = csv.reader(open('data.csv', 'r', newline=''))
         reader = csv.reader(data)
         sortdata = sorted(reader, key=lambda row: datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
-
-    # with open('data.csv', 'w', newline='') as data:
-        # writer = csv.writer(data, quoting=csv.QUOTE_ALL)
-        # writer.writerows(sortdata)
+    
+    # tells number of questions after new one
+    # with open('data.csv', 'r', newline='') as data:
+        # numline = sum(1 for row in data)
+        # print('number of question after: ' + str(numline))
--- questions/update_html.py
+++ questions/update_html.py
@@ -6,7 +6,9 @@ from datetime import datetime
 
 def update_html():
     
+    # 
     # update question index
+    #
     html_index_start = """
 		<!DOCTYPE html>
 		<html>
@@ -45,31 +47,42 @@ def update_html():
         </html>
     """
 
-    pages = []
+    pages = [] #list of question pages, to see which ones to remove
     with open('data.csv', "r+", newline='') as data:
         reader = csv.reader(data)
         index_mid = ""
         for row in reversed(list(reader)):
-            text = row[1]
-            text = text.replace("\\n", "")
+            # read data
             date = row[0]
+            text = row[1]
+            index = row[2]          
+            # prepare data
             datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
             date_format = datetime.strftime(datetime_obj, '%Y.%m.%d %I:%M %p')
-            date_html = datetime.strftime(datetime_obj, '%Y%m%d-%H%M%S')
-            id = 'question_' + date_html
+            # text = (row[1]).replace("\\n", "") #doing it this way before, for some reason
+            # using data, prepare HTML text
+            id = 'question_' + index
             file_name =  id + '.html'
             index_mid_part = html_index_mid_temp.replace("question_page", file_name)
             index_mid_part = index_mid_part.replace("date", date_format)
             index_mid_part = index_mid_part.replace("question_text", text)
             index_mid += index_mid_part
             pages.append(file_name)
-        
         html_str = html_index_start + index_mid + html_index_end
+        # Write HTML text to file
         Html_file = open("questions.html", "w")
         Html_file.write(html_str)
         Html_file.close()
-
+        
+    # remove any pages that need to be
+    files = os.listdir(os.curdir)
+    for f in files:
+        if f[:9] == 'question_':
+            if f not in pages:
+                os.remove(f)
+    #
     # update question pages
+    #
     html_page_temp = """
             <!DOCTYPE html>
             <html>
@@ -92,16 +105,19 @@ def update_html():
     with open('data.csv', "r+", newline='') as data:
         reader = csv.reader(data)
         for row in reader:
-            # write html files for each question page according to json
-            text = row[1]
-            text = text.replace("\\n", "")
+            # read data
             date = row[0]
+            text = row[1]
+            index = row[2]
+            # prepare data
             datetime_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
             date_format = datetime.strftime(datetime_obj, '%Y.%m.%d %I:%M %p')
-            date_html = datetime.strftime(datetime_obj, '%Y%m%d-%H%M%S')
-            id = 'question_' + date_html
+            # text = (row[1]).replace("\\n", "") #doing it this way before, for some reason
+            # using data, prepare HTML text
+            id = 'question_' + index
             file_name =  id + '.html'
-            if os.path.isfile(file_name):
+            # update HTML text if it exists, otherwise write new text
+            if os.path.isfile(file_name): 
                 print(file_name)
                 with open(file_name) as fp:
                     soup = BeautifulSoup(fp, 'lxml')
@@ -111,30 +127,12 @@ def update_html():
                     question.string = text
                     html_str = soup.prettify()
                     print(soup.prettify()) 
-                    # soup.time.replace_with(date)
-                    # soup.find("div", {"id": "question"}).replace_with(question)
-                    
-                    # html_str = BeautifulSoup(fp, 'lxml')
-                    # html_str = soup.replace("q_id", id)
-                    # html_str = html_str.replace("question_text", text)
-                    # html_str = html_str.replace("question_date", date)
-                
-                # new_title = id
-                # new_date = date
-                # new_question = 
-                # print(title, date, question)
-                # continue
             else:
                 html_str = html_page_temp.replace("q_id", id)
                 html_str = html_str.replace("question_text", text)
                 html_str = html_str.replace("question_date", date)
+            # write HTML text to file
             Html_file = open(file_name, "w")
             Html_file.write(html_str)
             Html_file.close()
-    
-    files = os.listdir(os.curdir)
-    for f in files:
-        if f[:9] == 'question_':
-            if f not in pages:
-                os.remove(f)
             
