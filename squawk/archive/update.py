9f021677f572955c7636a210a3dd64cba2178952
Austin Pursley <austin.t.pursley@gmail.com>
2019-06-16 17:11:48 -0400

    removed questions and squawk, because they are still WIP

--- squawk/archive/update.py
+++
@@ -1,9 +0,0 @@
-import csv
-from datetime import datetime
-
-with open('data.csv','r', newline='') as data:
-    reader = csv.reader(data)
-    for row in reader:
-        file_name = row[2] + ".html"
-        if not os.path.exists(file_name):
-            with open(file_name, 'w') as html_file:
