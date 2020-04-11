a55caca0ab94f189d43eac758dda867c685ff215
Austin Pursley <austin.t.pursley@gmail.com>
2020-04-11 15:58:45 -0400

    removed hikes

--- hikes/hikes_index.js
+++
@@ -1,8 +0,0 @@
-// Index template render
-$(document).ready(function() {
-	$.getJSON('data.json', function(data) {
-			var template = $('#hike_index_template').html();
-			var html = Mustache.to_html(template, data);
-			$('#templateArea').html(html);
-	});
-});
